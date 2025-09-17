337d91987665c17e55fafa67f7924902ef608ab0
MrMancs <marcell.szatmari2007@gmail.com>
2025-09-17 11:59:08 +0200

    Refactor to Alakzat

--- src/main/java/com/example/alakzat2javafx/AlakzatController.java
+++ src/main/java/com/example/alakzat2javafx/AlakzatController.java
@@ -3,7 +3,7 @@ package com.example.alakzat2javafx;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class AlakzatController {
     @FXML
     private Label welcomeText;
 
